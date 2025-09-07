7737112a6a4a7983c6f6be4fafd60172a18284a1
whyDoILoveThis <136662855+whyDoILoveThis@users.noreply.github.com>
2025-09-07 17:01:25 -0500

    improved usage example for copy txt btn

--- app/its-copy-btn/CodeSnipCopyBtnUsage.ts
+++ app/its-copy-btn/CodeSnipCopyBtnUsage.ts
@@ -1,3 +1,20 @@
 export const CodeSnipCopyBtnUsage = `
+// Simplest usage
 <ItsCopyBtn text={testText} />
+
+// Making use of the optional props
+ <ItsCopyBtn
+   btnText={
+      <span className="flex gap-1 items-center">
+        <ClipboardIcon /> Copy
+      </span>
+    }
+   copiedText={
+      <span className="flex gap-1 items-center">
+        <ClipboardCheckIcon /> Copied
+      </span>
+    }
+   classNames="btn-ghost"
+   text={code}
+  />
 `
